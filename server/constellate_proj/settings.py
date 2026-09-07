5b79f9dfbe244df1f81e7817a1f862cf3210f308
ericka <2017eerickson@gmail.com>
2026-09-07 12:54:03 -0400

    updated django settings and .env

--- server/constellate_proj/settings.py
+++ server/constellate_proj/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ge+i6pzp-)tn+$0(pe)3q#vp8thg%wd3qjj8*ub+ru5k5yh0us'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
